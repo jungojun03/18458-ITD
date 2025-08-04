de876f83fc5a591077612b804ddc2b06cf2a73f1
jungojun03 <damianjungo10@gmail.com>
2025-08-03 20:05:43 -0500

    Update HardwareSubsystem.java
    this is a test to see if the servos work or not . . .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commandbase/Subsystems/HardwareSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commandbase/Subsystems/HardwareSubsystem.java
@@ -18,15 +18,14 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @Config
 public class HardwareSubsystem {
-    public Servo claw, ZPitch;
-    public Servo leftArm, rightArm;
+    public Servo Claw, ZPitch;
     private DcMotor RTPSpec;
     private DcMotor extendoSlide;
     private DcMotor InvertextendoSlide;
     private DcMotorEx specArm;
     private Motor.Encoder encoder;
     public Servo specWrist, specClaw;
-    private Servo wrist;
+    private Servo Wrist;
 
     public HardwareMap hardwareMap;
 
@@ -85,14 +84,8 @@ public class HardwareSubsystem {
         this.telemetry = opMode.telemetry;
         this.hardwareMap = opMode.hardwareMap;
 
-        this.leftArm = (Servo) hardwareMap.get("leftArm");
-        this.rightArm = (Servo) hardwareMap.get("rightArm");
-
-        leftArm.setDirection(Servo.Direction.REVERSE);
-        rightArm.setDirection(Servo.Direction.FORWARD);
-
         this.claw = (Servo) hardwareMap.get("Claw");
-        this.ZPitch = (Servo) hardwareMap.get("ZPitch");
+        this.zpitch = (Servo) hardwareMap.get("ZPitch");
 
         this.RTPSpec = (DcMotor) hardwareMap.get("specArm");
 
@@ -128,31 +121,28 @@ public class HardwareSubsystem {
     }
 
     public void armGrab() {
-        leftArm.setPosition(GRAB_POSE);
-        rightArm.setPosition(GRAB_POSE);
+        Arm.setPosition(GRAB_POSE);
         targetPose = GRAB_POSE;
     }
 
     public void hover() {
-        leftArm.setPosition(SPEC_GRAB);
-        rightArm.setPosition(SPEC_GRAB);
+      Arm.setPosition(SPEC_GRAB);
         targetPose = SPEC_GRAB;
     }
 
     public void armReset() {
-        leftArm.setPosition(RESET_POSE);
-        rightArm.setPosition(RESET_POSE);
+      Arm.setPosition(RESET_POSE);
         targetPose = RESET_POSE;
     }
 
     public void SampClose() {
-        claw.setPosition(GRAB);
-        clawStateGrabbed = true;
+        Claw.setPosition(GRAB);
+        ClawStateGrabbed = true;
     }
 
     public void SampOpen() {
-        claw.setPosition(OPEN);
-        clawStateGrabbed = false;
+        Claw.setPosition(OPEN);
+        ClawStateGrabbed = false;
     }
     public void Zintake() {
         ZPitch.setPosition(ZGRAB);
